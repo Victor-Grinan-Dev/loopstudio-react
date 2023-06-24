339f11ef5acc2dbf8142b8df13547f246ec9ebe2
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-06-24 14:42:05 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { HashRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <HashRouter className="App">'
+     Hello madafaka!
+    </HashRouter> 
+     
+   
+  );
+}
+
+export default App;
